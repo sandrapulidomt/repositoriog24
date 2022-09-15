b99a0efbb73c95d1bbccd3724120199fa31cdfb1
Sandra Pulido <sandra.pulido.mt@usa.edu.co>
2022-09-14 19:10:52 -0500

    agregar ejercicio5.java

---
+++ src/main/java/proyecto24/mavenproject2/ejercicio5.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto24.mavenproject2;
+
+/**
+ *
+ * @author pulid
+ */
+public class ejercicio5 {
+    public static void main(String[] args) {
+        System.out.println("hola hubo cambios");
+       
+    }
+}
